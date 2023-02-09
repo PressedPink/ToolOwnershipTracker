2aa37c6db9f879cef183e8dd292af529a4a1a8c5
alexMFuller <alex_fuller@ymail.com>
2023-02-09 08:59:44 -0600

    added basic fields for the user model

--- ToolOwnershipTracker/base/models.py
+++ ToolOwnershipTracker/base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=40)
+    email = models.CharField(max_length=40, primary_key=True)
+    phoneNumber = models.CharField(max_length=20)
+    role = models.CharField(max_length=11)
+    password = models.CharField(max_length=20)
+    #Needs field for toolbox
