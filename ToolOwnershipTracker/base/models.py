a8ba017700aaa9c4c8df2e22216849cf2957d577
judas <judas.lane@gmail.com>
2023-02-14 19:33:12 -0600

    Updated user model

--- ToolOwnershipTracker/base/models.py
+++ ToolOwnershipTracker/base/models.py
@@ -7,5 +7,10 @@ class user(models.Model):
     firstName = models.CharField(max_length=20)
     lastName = models.CharField(max_length=20)
     email = models.CharField(max_length=40, primary_key=True, Unique=True)
-    role = models.IntegerField()  # radio buttons 1-3 for user type
+    role = models.CharField(max_length=1, choices=UserType.choices, default=UserType.user)
     password = models.CharField(max_length=32)  # set to 32 for size of MD5 Hash
+    address = models.CharField(max_length=300, default="")
+    phone = models.CharField(max_length=14, default="")
+
+    def __str__(self):
+        return self.username
