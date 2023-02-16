857ae665956d3619ff026bc3691acfe42ac2a04c
judas <judas.lane@gmail.com>
2023-02-16 17:19:35 -0600

    made methods smaller to aid in coding changing information

--- ToolOwnershipTracker/ToolOwnershipTracker/Users.py
+++ ToolOwnershipTracker/ToolOwnershipTracker/Users.py
@@ -9,72 +9,89 @@ from ToolOwnershipTracker.base.models import user
 
 
 class User():
-    def createUser(self, firstName, lastName, email, role, password, confirmPassword, address, phone):
-        if email is None:
-            raise Exception("Unique Email Required")
-        test = list(map(str, user.objects.filter(email=email)))
-        if test.length != 0:
-            raise Exception("User already exists")
-        if firstName is None:
-            raise Exception("First Name may not be left blank")
-        if lastName is None:
-            raise Exception("Last Name may not be left blank")
-        if role is None:
-            raise Exception("Must select a user type")
-        if address is None:
-            raise Exception("Address may not be left blank")
-        if phone is None:
-            raise Exception("Phone Number may not be left blank")
-        #phone length
-        if phone.length is 10:
-            raise Exception("Please include your country code")
-        if phone.length is 7:
-            raise Exception("Please include your country and area codes")
-        if phone.length is not 11:
-            raise Exception("Please enter a valid phone number")
-        tempDigit = True
-        for number in phone:
-            if not number.isnumeric():
-                tempDigit = False
-        if tempDigit:
-            raise Exception("Phone Number Invalid")
-        if not '@' & '.' in email:
-            raise Exception("Email is Invalid")
-        if password.length < 12:
-            raise Exception("Password must be at least 12 characters")
-        if not re.search('!|@|#|$|%|^|&|\\*|\\(|\\)|_|\\+|-|=', password):
-            raise Exception("Password must contain a symbol")
-        tempUpper = False
-        tempLower = False
-        tempDigit = False
-        for letter in password:
-            if letter.isupper():
-                tempUpper = True
-            if letter.islower():
-                tempLower = True
-            if letter.isdigit():
-                tempDigit = True
-        if not tempUpper:
-            raise Exception("Password must contain an uppercase letter")
-        if not tempLower:
-            raise Exception("Password must contain a lowercase letter")
-        if not tempDigit:
-            raise Exception("Password must contain a number")
-        if firstName in password:
-            raise Exception("Password may not contain any part of your name")
-        if password is not confirmPassword:
-            raise Exception("Passwords do not Match")
-        hashPass = hashlib.md5(password)
-        newUser = user(firstName, lastName,email, role, hashPass, address, phone)
+    def createUser(self, firstName, lastName, email, password, confirmPassword, address, phone):
+        checkEmail(self,email)
+        checkFirstName(self,firstName)
+        checkLastName(self,lastName)
+        checkAddress(self,address)
+        checkPhone(self,phone)
+        verifyPasswordRequirements(self,password,confirmPassword)
+        hashPass = hashPass(password)
+        newUser = user(firstName, lastName,email, 1, hashPass, address, phone) #1 = basic user
         newUser.save()
 
+        def checkAddress(self,address):
+            if address is None:
+                raise Exception("Address may not be left blank")
+
+        def checkFirstName(self, firstName):
+            if firstName is None:
+                raise Exception("First Name may not be left blank")
+
+        def checkLastName(self,lastName):
+            if lastName is None:
+                raise Exception("Last Name may not be left blank")
+
         # Possibly device these checks into submethods todo
+        def checkEmail(self,email):
+            if email is None:
+                raise Exception("Unique Email Required")
+            test = list(map(str, user.objects.filter(email=email)))
+            if test.length != 0:
+                raise Exception("User already exists")
+            if not '@' & '.' in email:
+                raise Exception("Email is Invalid")
+
+        def checkPhone(self,phone):
+            if phone is None:
+                raise Exception("Phone Number may not be left blank")
+            if phone.length is 10:
+                raise Exception("Please include your country code")
+            if phone.length is 7:
+                raise Exception("Please include your country and area codes")
+            if phone.length is not 11:
+                raise Exception("Please enter a valid phone number")
+            tempDigit = True
+            for number in phone:
+                if not number.isnumeric():
+                    tempDigit = False
+            if tempDigit:
+                raise Exception("Phone Number Invalid")
 
         def checkPassword(self, password):
-            pw = hashlib.md5(password)
-            if this.password is not pw:
+            pw = hashPass(password)
+            if self.password is not pw:
                 return False
             return True
+
+        def hashPass(self,password):
+            return hashlib.md5(password)
+
+        def verifyPasswordRequirements(self,password,confirmPassword):
+            if password.length < 12:
+                raise Exception("Password must be at least 12 characters")
+            if not re.search('!|@|#|$|%|^|&|\\*|\\(|\\)|_|\\+|-|=', password):
+                raise Exception("Password must contain a symbol")
+            tempUpper = False
+            tempLower = False
+            tempDigit = False
+            for letter in password:
+                if letter.isupper():
+                    tempUpper = True
+                if letter.islower():
+                    tempLower = True
+                if letter.isdigit():
+                    tempDigit = True
+            if not tempUpper:
+                raise Exception("Password must contain an uppercase letter")
+            if not tempLower:
+                raise Exception("Password must contain a lowercase letter")
+            if not tempDigit:
+                raise Exception("Password must contain a number")
+            if firstName in password:
+                raise Exception("Password may not contain any part of your name")
+            if password is not confirmPassword:
+                raise Exception("Passwords do not Match")
 
         def clearSessions(self):
             #todo clear all active sessions, set active to false
