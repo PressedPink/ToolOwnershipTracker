22db3cc74a4ba881ab8b7402641885774d583c95
alexMFuller <alex_fuller@ymail.com>
2023-02-20 16:42:12 -0600

    major-rebase2 	modified:   .vscode/launch.json 	renamed:    app/ToolOwnershipTracker/Users.py -> capstoneMain/ToolOwnershipTracker/Users.py 	renamed:    app/ToolOwnershipTracker/__init__.py -> capstoneMain/ToolOwnershipTracker/__init__.py 	renamed:    app/ToolOwnershipTracker/apps.py -> capstoneMain/ToolOwnershipTracker/apps.py 	renamed:    app/ToolOwnershipTracker/asgi.py -> capstoneMain/ToolOwnershipTracker/asgi.py 	renamed:    app/ToolOwnershipTracker/models.py -> capstoneMain/ToolOwnershipTracker/models.py 	renamed:    app/ToolOwnershipTracker/settings.py -> capstoneMain/ToolOwnershipTracker/settings.py 	renamed:    app/ToolOwnershipTracker/tests/acceptance tests/__init__.py -> capstoneMain/ToolOwnershipTracker/tests/acceptance tests/__init__.py 	renamed:    app/ToolOwnershipTracker/tests/unit tests/__init__.py -> capstoneMain/ToolOwnershipTracker/tests/unit tests/__init__.py 	renamed:    app/ToolOwnershipTracker/tests/unit tests/userTests.py -> capstoneMain/ToolOwnershipTracker/tests/unit tests/userTests.py 	renamed:    app/ToolOwnershipTracker/urls.py -> capstoneMain/ToolOwnershipTracker/urls.py 	renamed:    app/ToolOwnershipTracker/views.py -> capstoneMain/ToolOwnershipTracker/views.py 	renamed:    app/ToolOwnershipTracker/wsgi.py -> capstoneMain/ToolOwnershipTracker/wsgi.py 	renamed:    app/base/__init__.py -> capstoneMain/base/__init__.py 	renamed:    app/base/admin.py -> capstoneMain/base/admin.py 	renamed:    app/base/migrations/__init__.py -> capstoneMain/base/migrations/__init__.py 	renamed:    app/base/urls.py -> capstoneMain/base/urls.py 	renamed:    app/manage.py -> capstoneMain/manage.py 	renamed:    app/templates/profile.html -> capstoneMain/templates/profile.html 	renamed:    app/templates/signup.html -> capstoneMain/templates/signup.html 	renamed:    app/templates/untitled.html -> capstoneMain/templates/untitled.html

--- capstoneMain/ToolOwnershipTracker/wsgi.py
+++ capstoneMain/ToolOwnershipTracker/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ToolOwnershipTracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'ToolOwnershipTracker.settings')
+root_path = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, os.path.join(root_path, 'project_name'))
+sys.path.insert(0, root_path)
 
 application = get_wsgi_application()
