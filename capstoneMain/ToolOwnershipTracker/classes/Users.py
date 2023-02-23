6ac002b4f0a3dbbf92eaff2724b1e86afd192f3c
judas <judas.lane@gmail.com>
2023-02-22 19:34:37 -0600

    added colon

--- capstoneMain/ToolOwnershipTracker/classes/Users.py
+++ capstoneMain/ToolOwnershipTracker/classes/Users.py
@@ -120,7 +120,10 @@ class User():
         return True
 
     def editFirstName(self, firstName):
-        if checkFirstName(self,firstName):
+        if self.checkFirstName(self, firstName):
             self.firstName = firstName
             self.save()
+
+    def save(self):
+        pass
 
