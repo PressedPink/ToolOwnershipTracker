5b65d4a676f635d93725f2a30df3b844bc8d8d57
kino174 <70708859+kino174@users.noreply.github.com>
2023-02-22 19:50:58 -0600

    working EditUser logic!!! i think.

--- ToolOwnershipTracker/base/views.py
+++ ToolOwnershipTracker/base/views.py
@@ -11,22 +11,20 @@ class Profile(View):
 
         return render(request, "profile.html", {"currentUser": b})
 
-    #class EditUser(View):
-    #def get(self, request):
-        #m = request.session["email"]
-        #return render(request, "edituser.html")
-    
-    #def post(self, request):
-        #profile = Profile(email=request.session['email'])
-        #accountType = request.POST['type']
-        #password = request.POST['password']
-        #email = request.POST['email']
-        #phone = request.POST['phone']
-        #address = request.POST['address']
-        #userEdit = profile.editUser(password=str(password), email=str(email), phone=str(phone), address=str(address))
-        #if userEdit:
-            #return redirect("/edituser/")
-        #else:
-            #m = request.session["username"]
-            #a = request.session["accountType"]
-            #return render(request, "edituser.html", {"username": m, "accountType": a, "message": "invalid login"})
+class EditUser(View):
+    def get(self, request):
+        m = request.session["email"]
+        return render(request, "edituser.html", {"email": m})
+
+    def post(self, request):
+        profile = Profile(email=request.session['email'])
+        password = request.POST['password']
+        phone = request.POST['phone']
+        address = request.POST['address']
+        userEdited = profile.editUser(password=password, phone=phone, address=address)
+        if userEdited:
+            return redirect("/edituser/")
+        else:
+            m = request.session["email"]
+            a = request.session["accountType"]
+            return render(request, "edituser.html", {"email": m, "message": "invalid login"})
