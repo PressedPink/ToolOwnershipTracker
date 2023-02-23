e0dcec45854d2ee94b1b58a3b7f312c091d3f780
kino174 <70708859+kino174@users.noreply.github.com>
2023-02-22 19:49:54 -0600

    editUser function in Profile, working!!

--- ToolOwnershipTracker/classes/profile.py
+++ ToolOwnershipTracker/classes/profile.py
@@ -2,9 +2,9 @@ from base.models import User
 
 # Used for booleans.
 class Profile:
-    def __init__(self, username):
-        self.username = username
-        
-    def editUser(self, password, email, phone, address):
-        toEdit = User.objects.get(username=self.username)
-        toEdit = User.objects.update(password=password, email=email, phone=phone, address=address)
+    def __init__(self, email):
+        self.email = email
+
+    def editUser(self, password, phone, address):
+        User.objects.filter(email=self.email).update(password=password,phone=phone,address=address)
+        return True
