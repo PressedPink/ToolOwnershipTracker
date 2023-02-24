7725a1e58a948218aad3d051b19c7a1cb634b613
alexMFuller <alex_fuller@ymail.com>
2023-02-24 05:31:54 -0600

    fixes for demo

--- capstoneMain/settings.py
+++ capstoneMain/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-*!+wj-=^$515#m0s0epmp#(kkp7@q8zs!x-km^v@1=f(y(6lk1
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 ALLOWED_HOSTS = []
 
 GRAPH_MODELS = {
