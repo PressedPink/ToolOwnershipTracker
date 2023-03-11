e2d1442503d21391bb3c07299829c247080f1168
judas <judas.lane@gmail.com>
2023-03-11 10:28:06 -0600

    added remove tool from toolbox

--- capstoneMain/ToolOwnershipTracker/classes/Toolbox.py
+++ capstoneMain/ToolOwnershipTracker/classes/Toolbox.py
@@ -1,17 +1,35 @@
 from capstoneMain.ToolOwnershipTracker.Jobsite import Jobsite
 
 
+
+
 def Toolbox():
-    def createToolbox(self, id, owner):
-        isValidJobsite(id)
+    def createToolbox(self, ToolboxID, owner):
+        isValidJobsite(ToolboxID)
         Jobsite.isValid(owner)
-        tbx = Toolbox(self, id, owner)
+        tbx = Toolbox(self, ToolboxID, owner)
         tbx.save()
         return True
 
-
-    def isValidJobsite(self, id):
+    def isValidJobsite(self, ToolboxID):
         test = list(map(str, Jobsite.objects.filter(id=id)))
         if test.length == 0:
             return False
         return True
+
+    def addTool(self, tool):
+        isUnassigned(self,tool)
+
+    def removeTool(self,tool):
+        isUnassigned(self,tool)
+        self.remove.tools(tool)
+        Tool.remove.toolbox()
+
+    def isUnassigned(self, tool):
+        if tool.owner is not None:
+            raise Exception("This tool is already assigned to a User")
+        if tool.jobsite is not None:
+            raise Exception("This tool is already in another jobsite's toolbox")
+        return True
+
+
