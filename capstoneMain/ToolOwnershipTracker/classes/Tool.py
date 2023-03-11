d4db99ff2ca59c94d4bc16071d8cf5438c6d5283
judas <judas.lane@gmail.com>
2023-03-11 10:33:58 -0600

    moved function to new class

---
+++ capstoneMain/ToolOwnershipTracker/classes/Tool.py
@@ -0,0 +1,20 @@
+from capstoneMain.ToolOwnershipTracker.Users import User
+
+def Tool():
+
+    def createTool(self):
+        tool = Tool(self)
+        tool.save()
+
+    def deleteTool():
+
+    def isUnassigned(self, owner, jobsite):
+        if owner is not None:
+            raise Exception("This tool is already assigned to a User")
+        if jobsite is not None:
+            raise Exception("This tool is already in another jobsite's toolbox")
+        return True
+
+    def changeUser():
+
+    def changeLocation():
--- capstoneMain/ToolOwnershipTracker/classes/Toolbox.py
+++ capstoneMain/ToolOwnershipTracker/classes/Toolbox.py
@@ -1,6 +1,6 @@
 from capstoneMain.ToolOwnershipTracker.Jobsite import Jobsite
 
-
+from capstoneMain.ToolOwnershipTracker.classes import Tool
 
 
 def Toolbox():
@@ -18,18 +18,9 @@ def Toolbox():
         return True
 
     def addTool(self, tool):
-        isUnassigned(self,tool)
+        Tool.isUnassigned(self, tool)
 
-    def removeTool(self,tool):
-        isUnassigned(self,tool)
+    def removeTool(self, tool):
+        Tool.isUnassigned(self, tool)
         self.remove.tools(tool)
         Tool.remove.toolbox()
-
-    def isUnassigned(self, tool):
-        if tool.owner is not None:
-            raise Exception("This tool is already assigned to a User")
-        if tool.jobsite is not None:
-            raise Exception("This tool is already in another jobsite's toolbox")
-        return True
-
-
