9c3d7e4749c14c3e2cac2195631df355cc2a5895
Connor Nethen <connornethen258@gmail.com>
2023-03-22 14:57:17 -0500

    First Edit

---
+++ ToolOwnershipTracker/AcceptanceTests/ForgotPasswordTests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase, Client
+from capstoneMain.ToolOwnershipTracker.models import User
+
+class UserTestForgotPassword(TestCase):
+
+    def setup(self):
+        self.testClient = Client()
+        myuser = User(firstName="userfirst",
+                      lastName="userlast",
+                      email="email1@gmail.com",
+                      role="U",
+                      password="userpass",
+                      address="123 N Road St",
+                      phoneNumber="14141234567")
+        myuser.save()
+
