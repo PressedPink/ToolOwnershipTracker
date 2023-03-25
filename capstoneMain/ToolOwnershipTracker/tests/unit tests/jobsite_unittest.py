aae43ec787716ec6c887fdd4a734ba0b03dddb6e
judas <judas.lane@gmail.com>
2023-03-25 15:44:39 -0500

    added add Jobsite Unit Tests

--- capstoneMain/ToolOwnershipTracker/tests/unit tests/jobsite_unittest.py
+++ capstoneMain/ToolOwnershipTracker/tests/unit tests/jobsite_unittest.py
@@ -1,8 +1,45 @@
 from django.test import TestCase
 import capstoneMain.ToolOwnershipTracker.classes.Jobsite
 import capstoneMain.ToolOwnershipTracker.models
+from capstoneMain.ToolOwnershipTracker.models import Toolbox, Jobsite, User
+
 
 class addJobsiteTests(TestCase):
+
+    def setUp(self):
+        tempUser = User(firstName = "test", lastName = "test", email = "test", password = "test", address = "test", phone = "test")
+        tempUser.save()
+        tempToolbox = Toolbox(id="1", owner=tempUser)
+        tempToolbox.save()
+
+    def testPositiveWithAll(self, tempUser, tempToolbox):
+        tempJobsite = Jobsite(id = "1", owner = tempUser, title = "test", assigned = tempUser, toolbox = tempToolbox)
+        tempJobsite.save()
+        self.assertEquals(tempJobsite.id, 1)
+        self.assertEquals(tempJobsite.owner, tempUser)
+        self.assertEquals(tempJobsite.title, "test")
+        self.assertEquals(tempJobsite.assigned, tempUser)
+        self.assertEquals(tempJobsite.toolbox, tempToolbox)
+
+    def testPositiveWithoutAssigned(self, tempUser, tempToolbox):
+        tempJobsite = Jobsite(id="1", owner=tempUser, title="test", toolbox=tempToolbox)
+        tempJobsite.save()
+        self.assertEquals(tempJobsite.id, 1)
+        self.assertEquals(tempJobsite.owner, tempUser)
+        self.assertEquals(tempJobsite.title, "test")
+        self.assertIsNone (tempJobsite.assigned)
+        self.assertEquals(tempJobsite.toolbox, tempToolbox)
+
+    def testNegativeWithOwner(self, tempUser, tempToolbox):
+        tempJobsite = Jobsite(id="1", owner=tempUser, title="test", assigned = tempUser)
+        tempJobsite.save()
+        self.assertRaises (Exception)
+
+    def testNegativeWithToolbox(self):
+        tempJobsite = Jobsite(id="1", title="test", assigned=tempUser, toolbox=tempToolbox)
+        tempJobsite.save()
+        self.assertRaises (Exception)
+
 
 class removeJobsiteTests(TestCase):
 
