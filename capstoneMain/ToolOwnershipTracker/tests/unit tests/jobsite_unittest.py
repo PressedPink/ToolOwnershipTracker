8ef0f88eda7fb2bb085921a4dfec247962318c3b
judas <judas.lane@gmail.com>
2023-03-25 15:02:47 -0500

    added test classes

---
+++ capstoneMain/ToolOwnershipTracker/tests/unit tests/jobsite_unittest.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+import capstoneMain.ToolOwnershipTracker.classes.Jobsite
+import capstoneMain.ToolOwnershipTracker.models
+
+class addJobsiteTests(TestCase):
+
+class removeJobsiteTests(TestCase):
+
+class addToolTests(TestCase):
+
+class removeToolTests(TestCase):
+
+class editJobsiteTests(TestCase):
