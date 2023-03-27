63000c9a5a85c43624470bb3dc4e759e9ac9054a
kino174 <70708859+kino174@users.noreply.github.com>
2023-03-26 19:01:23 -0500

    Adding urls to the project

--- capstoneMain/ToolOwnershipTracker/urls.py
+++ capstoneMain/ToolOwnershipTracker/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 
 
 import base
-from ToolOwnershipTracker.views import Profile, Login
+from ToolOwnershipTracker.views import Profile, Login, SignUp, EditUser
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', Profile.as_view()),
+    path('signup/', SignUp.as_view()),
+    path('edituser/', EditUser.as_view()),
     path('', Login.as_view(), name='LoginHTML'),
 ]
