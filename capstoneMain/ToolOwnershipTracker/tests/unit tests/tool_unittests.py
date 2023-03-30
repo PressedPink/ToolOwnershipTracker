976967f4d72d6f8fb40ad5a013572dced246eabb
judas <judas.lane@gmail.com>
2023-03-30 14:35:09 -0500

    changeowner tests

--- capstoneMain/ToolOwnershipTracker/tests/unit tests/tool_unittests.py
+++ capstoneMain/ToolOwnershipTracker/tests/unit tests/tool_unittests.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest import TestCase
 
-from capstoneMain.ToolOwnershipTracker.models import Tool, User
+from capstoneMain.ToolOwnershipTracker.models import Tool, User, Jobsite
 
 
 class testCreateTool(TestCase):
@@ -23,3 +23,25 @@ class testRemoveTool(TestCase):
         self.tempTool.toolbox = self.admin.email
         self.tempTool.save()
         self.assertRaises(Exception, self.tempTool)
+
+
+class testChangeOwner(TestCase):
+    def setup(self):
+        admin = User(firstName="test", lastName="test", email="test", password="test", address="test", phone="test")
+        admin.save()
+        tempTool = Tool(id="1")
+        tempTool.save()
+        tempJobsite = Jobsite(id="1", owner=admin, title="test")
+        tempJobsite.save()
+
+    def testPositiveUser(self):
+        self.assertTrue(Tool.changeOwner(self.tempTool, self.admin))
+
+    def testPositiveJobsite(self):
+        self.assertTrue(Tool.changeOwner(self.tempTool, self.tempJobsite.id))
+
+    def testNegativeNotaValidUser(self):
+        self.assertRaises(Exception, Tool.changeOwner(self.tempTool, "@"))
+
+    def testNegativeNotaValidJobsite(self):
+        self.assertRaises(Exception, Tool.changeOwner(self.tempTool, ""))
