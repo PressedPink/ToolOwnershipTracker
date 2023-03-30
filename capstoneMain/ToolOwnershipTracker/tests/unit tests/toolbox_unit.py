b9a6c2e17911722eb6082fa4d06819973cf8e05a
judas <judas.lane@gmail.com>
2023-03-30 14:00:55 -0500

    addToolsTest

--- capstoneMain/ToolOwnershipTracker/tests/unit tests/toolbox_unit.py
+++ capstoneMain/ToolOwnershipTracker/tests/unit tests/toolbox_unit.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 import capstoneMain.ToolOwnershipTracker.classes.Jobsite
 import capstoneMain.ToolOwnershipTracker.models
-from capstoneMain.ToolOwnershipTracker.models import Toolbox, Jobsite, User
+from capstoneMain.ToolOwnershipTracker.models import Toolbox, Jobsite, User, Tool
 
 
 class addToolboxTests(TestCase):
@@ -24,3 +24,30 @@ class addToolboxTests(TestCase):
         tempBox = Toolbox(id="1", owner=self.admin)
         tempBox.save()
         self.assertRaises(Exception, Toolbox.createToolbox(self, self.admin.email))
+
+
+class addToolTests(TestCase):
+
+    def setup(self):
+        tempUser = User(firstName="test", lastName="test", email="test", password="test", address="test", phone="test")
+        tempUser.save()
+        tempJobsite = Jobsite(id="1", owner=tempUser, title="test")
+        tempJobsite.save()
+        tempTool = Tool(id="1")
+        tempTool.save()
+
+    def addToolPositive(self):
+        self.assertTrue(Toolbox.addTool(self.tempToolbox, self.tempTool))
+
+    def addToolNegativeDoesNotExist(self):
+        self.assertRaises(Exception, self.tempToolbox, "")
+
+    def addToolNegativeAlreadyAssignedToJobsite(self):
+        self.tempTool.toolbox = self.tempJobsite.id
+        self.tempTool.save()
+        self.assertRaises(Exception, Toolbox.addTool(self.tempToolbox, self.tempTool))
+
+    def addToolNegativeAlreadyAssignedToUser(self):
+        self.tempTool.toolbox = self.tempUser.email
+        self.tempTool.save()
+        self.assertRaises(Exception, Toolbox.addTool(self.tempToolbox, self.tempTool))
