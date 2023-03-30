19e0667c9b61479732581201ee8f0dadc3685928
judas <judas.lane@gmail.com>
2023-03-30 14:19:10 -0500

    removeAllTools

--- capstoneMain/ToolOwnershipTracker/tests/unit tests/toolbox_unit.py
+++ capstoneMain/ToolOwnershipTracker/tests/unit tests/toolbox_unit.py
@@ -35,19 +35,49 @@ class addToolTests(TestCase):
         tempJobsite.save()
         tempTool = Tool(id="1")
         tempTool.save()
+   c
 
     def addToolPositive(self):
-        self.assertTrue(Toolbox.addTool(self.tempToolbox, self.tempTool))
+        self.assertTrue(Toolbox.addTool(self.tempBox, self.tempTool))
 
     def addToolNegativeDoesNotExist(self):
-        self.assertRaises(Exception, self.tempToolbox, "")
+        self.assertRaises(Exception, self.tempBox, "")
 
     def addToolNegativeAlreadyAssignedToJobsite(self):
         self.tempTool.toolbox = self.tempJobsite.id
         self.tempTool.save()
-        self.assertRaises(Exception, Toolbox.addTool(self.tempToolbox, self.tempTool))
+        self.assertRaises(Exception, Toolbox.addTool(self.tempBox, self.tempTool))
 
     def addToolNegativeAlreadyAssignedToUser(self):
         self.tempTool.toolbox = self.tempUser.email
         self.tempTool.save()
-        self.assertRaises(Exception, Toolbox.addTool(self.tempToolbox, self.tempTool))
+        self.assertRaises(Exception, Toolbox.addTool(self.tempBox, self.tempTool))
+
+class removeAllTools(TestCase):
+
+    def setup(self):
+        tempUser = User(firstName="test", lastName="test", email="test", password="test", address="test", phone="test")
+        tempUser.save()
+        tempJobsite = Jobsite(id="1", owner=tempUser, title="test")
+        tempJobsite.save()
+        tempTool = Tool(id="1", owner = tempJobsite.id)
+        tempTool.save()
+        tempBox = Toolbox(id="1", owner=self.admin)
+        tempBox.save()
+
+    def testPositiveOneTool(self):
+        self.assertTrue(Toolbox.removeAllTools(self.tempToolbox))
+
+    def testPositiveMultipleTools(self):
+        self.tempToolbox.tools.add(self.tempTool2)
+        self.tempToolbox.save()
+        self.assertTrue(Toolbox.removeAllTools(self.tempToolbox))
+
+    def testNegativeToolIsAssigned(self):
+        self.tempTool.toolbox=self.tempUser.email
+        self.tempTool.save()
+        self.tempToolbox.tools = None
+        self.tempToolbox.save()
+        self.tempToolbox.add(self.tempTool)
+        self.tempToolbox.save()
+        self.assertRaises(Exception,Toolbox.removeAllTools(self.tempToolbox))
