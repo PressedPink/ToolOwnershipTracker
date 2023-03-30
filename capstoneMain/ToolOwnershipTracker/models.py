4fb120a1476aaea05c84128df3031ffae9489790
judas <judas.lane@gmail.com>
2023-03-29 19:36:17 -0500

    corrected models

--- capstoneMain/ToolOwnershipTracker/models.py
+++ capstoneMain/ToolOwnershipTracker/models.py
@@ -14,7 +14,7 @@ class UserType(models.TextChoices):
 # defines the user model, which contains the following fields: username, password, accountType, email, address,
 # phone number and active status
 
-
+# defines a User of any type
 class User(models.Model):
     firstName = models.CharField(max_length=20)
     lastName = models.CharField(max_length=20)
@@ -25,26 +25,27 @@ class User(models.Model):
     password = models.CharField(max_length=32)
     address = models.CharField(max_length=300, default="")
     phone = models.CharField(max_length=14, default="")
+    #records if user is active for security purposes
     active = models.BooleanField
 
-
+#defines a Jobsite
 class Jobsite(models.Model):
     id = models.ForeignKey(unique=True, primary_key=True)
+    #dictates supervisor that can view
     owner = models.CharField(User, on_delete=models.CASCADE, null=False)
     title = models.CharField(max_length=40)
+    #dictates users that can view
     assigned = models.CharField(User, on_delete=models.CASCADE, null=True)
-    toolbox = models.CharField(Toolbox, on_delete=models.CASCADE, null=False)
 
-
+#defines a toolbox for a Jobsite OR a User -- should NOT have both
+#noteownerANDjobsite should not BOTH be null, will verify and address in logic
 class Toolbox(models.Model):
     id = models.ForeignKey(unique=True, primary_key=True)
     tools = models.CharField(User, on_delete=models.CASCADE, null=True)
-    owner = models.CharField(User, on_delete=models.CASCADE, null=False)
+    owner = models.CharField(User, on_delete=models.CASCADE, null=True)
     jobsite = models.CharField(Jobsite, on_delete=models.CASCADE, null=True)
 
-
-
+#defines a tool. Tools WITHOUT a toolbox are not assigned to a user OR a jobsite
 class Tool(models.Model):
     id = models.ForeignKey(unique=True, primary_key=True)
     toolbox = models.CharField(Toolbox, on_delete=models.CASCADE, null=True)
-    user = models.CharField(User, on_delete=models.CASCADE, null=True)
