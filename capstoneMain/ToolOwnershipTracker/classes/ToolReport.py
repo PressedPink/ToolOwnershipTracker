a0ae684b1288f0e848928149a0b8af6163bacd67
judas <judas.lane@gmail.com>
2023-04-04 20:14:14 -0500

    Start of ToolReport

---
+++ capstoneMain/ToolOwnershipTracker/classes/ToolReport.py
@@ -0,0 +1,15 @@
+
+
+
+def ToolReport():
+
+    def createToolReport(self):
+
+    def deleteReport(self):
+
+    #add impacted users
+    #remove impacted users
+    #add impacted tools
+    #remove impacted users
+    #change description
+    #change report types
