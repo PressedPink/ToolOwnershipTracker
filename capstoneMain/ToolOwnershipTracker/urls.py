d64cb90643ecf070093dbf275010803884d3c7be
Connor Nethen <connornethen258@gmail.com>
2023-04-06 19:36:44 -0500

    edit 7. Getting there

--- capstoneMain/ToolOwnershipTracker/urls.py
+++ capstoneMain/ToolOwnershipTracker/urls.py
@@ -4,7 +4,8 @@ from django.contrib.auth import views as auth_views
 from django.urls import include
 
 import base
-from ToolOwnershipTracker.views import Profile, Login, PasswordReset, PasswordResetSent, PasswordResetForm, PasswordResetDone, SignUp, Jobsites, editUsers, UserToolboxes, viewToolbox
+from ToolOwnershipTracker.views import Profile, Login, PasswordReset, PasswordResetSent, PasswordResetForm,\
+    PasswordResetDone, SignUp, Jobsites, editUsers, UserToolboxes, viewToolbox, createJobsite, editJobsite
 
 
 urlpatterns = [
@@ -20,6 +21,8 @@ urlpatterns = [
          name='password_reset_done'),
     path('signup/', SignUp.as_view(), name='signup'),
     path('jobsites/', Jobsites.as_view(), name='jobsites'),
+    path('createJobsite/', createJobsite.as_view(), name='createJobsite'),
+    path('editJobsite/', editJobsite.as_view(), name='editJobsite'),
     path('edituser/', editUsers.as_view(), name="edituser"),
     path('userToolboxes/', UserToolboxes.as_view(), name='userToolboxes'),
     path('viewToolbox/', viewToolbox.as_view(), name='viewToolbox'),
