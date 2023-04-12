6e244878cc7742d79e01ca85b25d840d40fd8d73
kino174 <lw021201@gmail.com>
2023-04-12 15:04:23 -0500

    Some reason allowed hosts needed to be changed

--- capstoneMain/settings.py
+++ capstoneMain/settings.py
@@ -33,7 +33,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 
 ]
-ALLOWED_HOSTS = ["192.168.0.210"]
+ALLOWED_HOSTS = ["192.168.0.210", "127.0.0.1"]
 
 GRAPH_MODELS = {
     'all_applications': True,
