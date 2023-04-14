a3e7f200942a3b0bc42d3ce02a0f13bee0ec8f32
kino174 <lw021201@gmail.com>
2023-04-14 15:04:22 -0500

    forms.py added additional fields, may need more

--- capstoneMain/ToolOwnershipTracker/forms.py
+++ capstoneMain/ToolOwnershipTracker/forms.py
@@ -4,11 +4,10 @@ from ToolOwnershipTracker.models import ToolReport, Tool, Toolbox, Jobsite, User
 class ReportForm(forms.ModelForm):
     class Meta:
         model = ToolReport
-        fields = ('id', 'jobsite', 'toolbox', 'tool')
+        fields = ('reportType','jobsite', 'toolbox', 'tool', 'name','description')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.fields['toolbox'].queryset = Toolbox.objects.none()
 
         
@@ -16,19 +15,19 @@ class ReportForm(forms.ModelForm):
         if 'jobsite' in self.data:
             try:
                 jobsite_id = int(self.data.get('jobsite'))
-                self.fields['toolbox'].queryset = Toolbox.objects.filter(jobsite_id=jobsite_id).order_by('id')
+                self.fields['toolbox'].queryset = Toolbox.objects.filter(jobsite_id=jobsite_id).order_by('name')
             except (ValueError, TypeError):
                 pass  # invalid input from the client; ignore and fallback to empty Toolbox queryset
         elif self.instance.pk:
-            self.fields['toolbox'].queryset = self.instance.jobsite.toolbox_set.order_by('id')
+            self.fields['toolbox'].queryset = self.instance.jobsite.toolbox_set.order_by('name')
 
         self.fields['tool'].queryset = Tool.objects.none()
         if 'toolbox' in self.data:
             try:
                 toolbox_id = int(self.data.get('toolbox'))
-                self.fields['tool'].queryset = Tool.objects.filter(toolbox_id=toolbox_id).order_by('id')
+                self.fields['tool'].queryset = Tool.objects.filter(toolbox_id=toolbox_id).order_by('name')
             except (ValueError, TypeError):
                 pass  # invalid input from the client; ignore and fallback to empty Toolbox queryset
         elif self.instance.pk:
-            self.fields['tool'].queryset = self.instance.toolbox.tool.order_by('id')
+            self.fields['tool'].queryset = self.instance.toolbox.tool.order_by('name')
 
