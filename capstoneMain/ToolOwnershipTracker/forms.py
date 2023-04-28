d8a79d4058f1dd5ea2901b5d54f55a3e3ff0c67a
kino174 <lw021201@gmail.com>
2023-04-28 18:05:05 -0500

    form for user toolbox tool report

--- capstoneMain/ToolOwnershipTracker/forms.py
+++ capstoneMain/ToolOwnershipTracker/forms.py
@@ -10,35 +10,23 @@ class TimeInput(forms.TimeInput):
 class ReportForm(forms.ModelForm):
     class Meta:
         model = ToolReport
-        fields = ('reportType','reporter', 'jobsite', 'toolbox', 'tool', 'topic', 'created', 'time', 'description')
+        fields = ('reportType','reporter', 'toolbox', 'tool', 'topic', 'created', 'time', 'description')
         widgets = {
           'description': Textarea(attrs={'size':'20'}),
           'created': DateInput(),
-          'time': TimeInput()
+          'time': TimeInput(),
+          'reporter': forms.HiddenInput(),
+          'toolbox': forms.HiddenInput(),
         }
     def __init__(self, *args, **kwargs):
         self.reporter = kwargs.pop('reporter')
+        self.toolbox = kwargs.pop('toolbox')
         super(ReportForm, self).__init__(*args, **kwargs)
-        self.fields['reporter'].queryset = User.objects.filter(
-            email=self.reporter)
+
+        self.fields['reporter'].initial = self.reporter
+        self.fields['toolbox'].initial = self.toolbox
+
+        self.fields['reporter'].widget.attrs['disabled'] = True
+        self.fields['toolbox'].widget.attrs['disabled'] = True
         self.fields['description'].widget.attrs['size'] = 20
-        self.fields['toolbox'].queryset = Toolbox.objects.none()
-        if 'jobsite' in self.data:
-            try:
-                jobsite_id = int(self.data.get('jobsite'))
-                self.fields['toolbox'].queryset = Toolbox.objects.filter(jobsite_id=jobsite_id).order_by('name')
-            except (ValueError, TypeError):
-                pass  # invalid input from the client; ignore and fallback to empty Toolbox queryset
-        elif self.instance.pk:
-            self.fields['toolbox'].queryset = self.instance.jobsite.toolbox_set.order_by('name')
-
-        self.fields['tool'].queryset = Tool.objects.none()
-        if 'toolbox' in self.data:
-            try:
-                toolbox_id = int(self.data.get('toolbox'))
-                self.fields['tool'].queryset = Tool.objects.filter(toolbox_id=toolbox_id).order_by('name')
-            except (ValueError, TypeError):
-                pass  # invalid input from the client; ignore and fallback to empty Toolbox queryset
-        elif self.instance.pk:
-            self.fields['tool'].queryset = Tool.objects.all()
-
+        self.fields['tool'].queryset = Tool.objects.filter(toolbox = self.toolbox)
