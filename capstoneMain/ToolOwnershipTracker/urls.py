18fc5f8f2bbe83cffe187a7790ef72cd37f8561a
kino174 <lw021201@gmail.com>
2023-04-28 19:40:53 -0500

    rebasing from MichaelBranch3.0

--- capstoneMain/ToolOwnershipTracker/urls.py
+++ capstoneMain/ToolOwnershipTracker/urls.py
@@ -2,25 +2,37 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.urls import include
+from django.views.generic import RedirectView
 
 import base
-
-from ToolOwnershipTracker.views import Profile, Login, PasswordReset, PasswordResetSent, PasswordResetForm, PasswordResetDone, SignUp, Jobsites, editUsers
-
+from . import views
+from ToolOwnershipTracker.views import (
+     Profile, Login, PasswordReset, PasswordResetSent, 
+     PasswordResetForm, PasswordResetDone, SignUp, Jobsites, editUsers, viewJobsitesSuperAdmin,
+)
 
 urlpatterns = [
-    path('', include('pwa.urls')),
-    path('admin/', admin.site.urls),
-    path('profile/', Profile.as_view()),
-    path('password_reset/', PasswordReset.as_view(), name='password-reset'),
-    path('password_reset_sent/', PasswordResetSent.as_view(),
-         name='password_reset_sent'),
-    path('password_reset_form/<str:token>/',
-         PasswordResetForm.as_view(), name='password_reset_form'),
-    path('password_reset_done/', PasswordResetDone.as_view(),
-         name='password_reset_done'),
-    path('signup/', SignUp.as_view(), name='signup'),
-    path('jobsites/', Jobsites.as_view(), name='jobsites'),
-    path('edituser/', editUsers.as_view(), name="edituser"),
-    path('', Login.as_view(), name='LoginHTML'),
+     path('', include('pwa.urls')),
+     path('admin/', admin.site.urls),
+     path('profile/', Profile.as_view()),
+     path('password_reset/', PasswordReset.as_view(), name='password-reset'),
+     path('password_reset_sent/', PasswordResetSent.as_view(),
+          name='password_reset_sent'),
+     path('password_reset_form/<str:token>/',
+          PasswordResetForm.as_view(), name='password_reset_form'),
+     path('password_reset_done/', PasswordResetDone.as_view(),
+          name='password_reset_done'),
+     path('signup/', SignUp.as_view(), name='signup'),
+     path('jobsites/', Jobsites.as_view(), name='jobsites'),
+     path('edituser/', editUsers.as_view(), name="edituser"),
+     path('', Login.as_view(), name='LoginHTML'),
+     path('jobsiteToolsAsSA/', viewJobsitesSuperAdmin.as_view(), name='jobsiteSA'),
+
+     path('toolReport/', views.ReportListView.as_view(), name='report_changelist'),
+     path('add/', views.ReportCreateView.as_view(), name='report_add'),
+     path('update/<int:pk>/', views.ReportUpdateView.as_view(), name='report_change'),
+     #path('ajax/load-toolbox/', views.load_toolbox, name='ajax_load_toolbox'),
+     path('ajax/load-tool/', views.load_tool, name='ajax_load_tool'),
+     path('delete/<int:pk>/', views.delete_object_function, name='delete_object'),
+     
 ]
