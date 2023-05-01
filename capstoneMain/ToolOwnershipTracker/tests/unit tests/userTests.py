354d8ed271d356291dd23d1ab05494586afd39d2
judas <judas.lane@gmail.com>
2023-05-01 13:09:25 -0500

    added createuser test

--- capstoneMain/ToolOwnershipTracker/tests/unit tests/userTests.py
+++ capstoneMain/ToolOwnershipTracker/tests/unit tests/userTests.py
@@ -4,14 +4,22 @@ from django.test import TestCase
 import unittest
 import django
 django.setup()
+from capstoneMain.ToolOwnershipTracker.models import User
+import Users
+
 
 class testCreateUser(TestCase):
 
     def setup(self):
+        tempUser = User(firstName = 'test',lastName='smith',email ='test@gmail.com', role = 'U', address ='test', password = 'test',
+                        phone = '11111111111', forgot_password_token = '54')                                                                                                                                                                           '5')
 
     def createUserFail(self):
+        self.assertRaises(Exception,Users.createUser('test','test','test@gmail.com', 'test', 'test', 'test', '11111111111'))
+
 
     def createUserSuccess(self):
+        self.assertTrue(Users.createUser('test','test','test2@gmail.com', 'test', 'test', 'test', '11111111111')))
 
 class editUserInfo(TestCase):
 
