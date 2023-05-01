8b8c5eed961454dc75e803c2133164002de1af9a
judas <judas.lane@gmail.com>
2023-05-01 13:21:22 -0500

    added change password

--- capstoneMain/ToolOwnershipTracker/tests/unit tests/userTests.py
+++ capstoneMain/ToolOwnershipTracker/tests/unit tests/userTests.py
@@ -24,10 +24,31 @@ class testCreateUser(TestCase):
 class editUserInfo(TestCase):
 
     def setup(self):
+        tempUser = User(firstName='test', lastName='smith', email='test@gmail.com', role='U', address='test',
+                        password='test',
+                        phone='11111111111', forgot_password_token='54')
 
     def changePasswordFail(self):
+        #is not 12 characters
+        self.assertRaises(Exception, Users.changePassword(tempUser.email, 'Potato1000!', 'Potato1000!'))
+        #missing special character
+        self.assertRaises(Exception, Users.changePassword(tempUser.email, 'Potato100000', 'Potato100000'))
+        #missing uppercase character
+        self.assertRaises(Exception, Users.changePassword(tempUser.email, 'potato!10000', 'potato!10000'))
+        #missing lowercase character
+        self.assertRaises(Exception, Users.changePassword(tempUser.email, 'POTATO!10000', 'POTATO!10000'))
+        #missing number
+        self.assertRaises(Exception, Users.changePassword(tempUser.email, 'POTATo!!!!!!', 'POTATo!!!!!!'))
+        #passwords do not match
+        self.assertRaises(Exception, Users.changePassword(tempUser.email, 'potatO10000!', 'Tomato10000!'))
+        #contains users name
+        self.assertRaises(Exception, Users.changePassword(tempUser.email, 'test10000000', 'test10000000'))
+        #email is invalid
+        self.assertRaises(Exception, Users.changePassword('fake@gmail.com', 'Potato1000!!', 'Potato1000!!'))
 
     def changePasswordSuccess(self):
+        self.assertTrue(Users.changePassword(tempUser.email, 'Potato1000!!', 'Potato1000!!')
+
 
     def changePhoneFail(self):
 
