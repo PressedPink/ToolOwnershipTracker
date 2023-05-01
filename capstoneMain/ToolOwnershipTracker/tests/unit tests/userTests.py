234d7e64bbedd1558fbebac0493514c91bdb5e54
judas <judas.lane@gmail.com>
2023-05-01 13:02:04 -0500

    added usertests classes

--- capstoneMain/ToolOwnershipTracker/tests/unit tests/userTests.py
+++ capstoneMain/ToolOwnershipTracker/tests/unit tests/userTests.py
@@ -4,3 +4,35 @@ from django.test import TestCase
 import unittest
 import django
 django.setup()
+
+class testCreateUser(TestCase):
+
+    def setup(self):
+
+    def createUserFail(self):
+
+    def createUserSuccess(self):
+
+class editUserInfo(TestCase):
+
+    def setup(self):
+
+    def changePasswordFail(self):
+
+    def changePasswordSuccess(self):
+
+    def changePhoneFail(self):
+
+    def changePhoneSuccess(self):
+
+    def changeRoleSuccess(self):
+
+    def changeRoleFail(self):
+
+    def changeAddressFail(self):
+
+    def changeAddressSuccess(self):
+
+    def changeNameFail(self):
+
+    def changeNameSuccess(self):
